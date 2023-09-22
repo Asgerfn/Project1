62507d2b862976f9f94bde2148afac99e252b0d1
Asgerfn <74596274+Asgerfn@users.noreply.github.com>
2023-09-22 12:31:15 +0200

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,12 +1,1 @@
-import pandas as pd
-
-sti = "C:\\Users\\Asger Nielsen\\firms.csv"
-df = pd.read_csv(sti)
-
-
-desired_years = [1968, 1969, 1970]
-mask = df['year'].isin(desired_years)
-
-filtered_df = df[mask]
-
-print(filtered_df)
+"hej alle sammen, denne fil er ligemeget :) "
