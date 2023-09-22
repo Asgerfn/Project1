0e72ea723d8d14f75f679c9a6ec08eab1d075d78
Asger Feldbæk Nielsen <asger.feldbaek@gmail.com>
2023-09-22 10:20:18 +0200

    j
    c

---
+++ project1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+sti = "C:\\Users\\Asger Nielsen\\firms.csv"
+df = pd.read_csv(sti)
+
+
+desired_years = [1968, 1969, 1970]
+mask = df['year'].isin(desired_years)
+
+# Use the mask to filter the DataFrame
+filtered_df = df[mask]
+
+print(filtered_df)
